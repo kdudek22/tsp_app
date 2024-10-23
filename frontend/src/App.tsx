0080aaa1121c97bbcb53752494ad2029126cb555
omnibus1 <appleofeden8686@gmail.com>
2024-10-24 00:09:28 +0200

    :fire:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,52 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMapEvents, Popup} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useState } from 'react';
 
+
+const ClickListener = ({onClick}) => {
+  const map = useMapEvents({
+    click(e){
+      onClick(e.latlng)
+    }
+  })
+  return null
+}
 
 function App() {
 
+  const [waypoints, setWaypoints] = useState([]);
+
+
+  const addWaypoint = (waypoint) => {
+    setWaypoints([...waypoints, waypoint])
+    console.log(waypoints)
+  }
+
+
   return (
     <>
-      <div className='bg-slate-800 w-screen h-screen flex justify-center py-8'>
-        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '40vh', width: '90vw'}}>
+      <div className='w-screen h-screen flex justify-center pb-8'>
+        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100vh', width: '100vw'}}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
+          <ClickListener onClick={addWaypoint}/>
+
+          {waypoints.map((waypoint, index) => (
+            <Marker position={waypoint}></Marker>
+          ))}
+
         </MapContainer>
+      </div>
+      <div className="absolute top-0 left-0 z-50 h-screen" style={{zIndex: 999}}>
+        <div className='my-20 overflow-y-scroll'>
+          <div className="">
+            {waypoints.map((waypoint, index) => (
+                <p>Waypoint: {index} {waypoint.lat} - {waypoint.lng}</p>
+              ))}
+          </div>
+        </div>
       </div>
     </>
   )
