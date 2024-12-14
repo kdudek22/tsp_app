438a79d3d264d40e2e199e4394fc6a349410adcc
= <=>
2024-12-14 18:34:51 +0100

    moved api token to env variable

--- ORS_backend/src/main/kotlin/ors_backend/ORS_backend/TSPController.kt
+++ ORS_backend/src/main/kotlin/ors_backend/ORS_backend/TSPController.kt
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.*
 @CrossOrigin(origins = ["*"])
 @RestController
 @RequestMapping("/api")
-class TSPController {
+class TSPController(private val httpService: HttpService) {
 
     @PostMapping("/matrix")
     fun getDurationMatrix(@RequestParam(name="transport_type", defaultValue="driving-car") transportType: String, @RequestBody waypoints: List<Waypoint>): Map<String, Any> {
         val transformedWaypoints = waypoints.map{arrayOf(it.lng, it.lat)}
 
-        val response = sendPostRequest("https://api.openrouteservice.org/v2/matrix/${transportType}",
+        val response = httpService.sendPostRequest("https://api.openrouteservice.org/v2/matrix/${transportType}",
             jacksonObjectMapper().writeValueAsString(mapOf("locations" to transformedWaypoints, "metrics" to arrayOf("distance", "duration"))))
 
         val responseString = response.body?.string() ?: "No response body"
@@ -25,7 +25,7 @@ class TSPController {
     fun getRoute(@RequestParam(name="transport_type", defaultValue="driving-car") transportType: String, @RequestBody waypoints: List<Waypoint>): Map<String, Any> {
         val transformedWaypoints = waypoints.map{arrayOf(it.lng, it.lat)}
 
-        val response = sendPostRequest("https://api.openrouteservice.org/v2/directions/${transportType}/geojson",
+        val response = httpService.sendPostRequest("https://api.openrouteservice.org/v2/directions/${transportType}/geojson",
             jacksonObjectMapper().writeValueAsString(mapOf("coordinates" to transformedWaypoints, "radiuses" to arrayOf(-1))))
 
 
--- ORS_backend/src/main/kotlin/ors_backend/ORS_backend/Utils.kt
+++ ORS_backend/src/main/kotlin/ors_backend/ORS_backend/Utils.kt
@@ -4,17 +4,27 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
 
-fun sendPostRequest(url: String, body: String): Response {
-    val client = OkHttpClient()
-    val requestBody = body.toRequestBody()
 
-    val request = Request.Builder()
-        .url(url)
-        .post(requestBody)
-        .addHeader("Authorization", "5b3ce3597851110001cf62487142604a89c04e778b3add6bf1387dee")
-        .addHeader("Content-Type", "application/json")
-        .build()
+@Service
+class HttpService{
 
-    return client.newCall(request).execute()
+    @Value("\${ors_api_key}")
+    private lateinit var apiKey: String
+
+    fun sendPostRequest(url: String, body: String): Response {
+        val client = OkHttpClient()
+        val requestBody = body.toRequestBody()
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .addHeader("Authorization", apiKey)
+            .addHeader("Content-Type", "application/json")
+            .build()
+
+        return client.newCall(request).execute()
+    }
 }
